4e8cc085e8639361ae134f8013e089eab6148a3d
matthewchase <mattchasebeaty+git@gmail.com>
2022-11-02 10:34:37 -0400

    sample server model

---
+++ onewheel-blog/app/models/posts.server.ts
@@ -0,0 +1,13 @@
+export async function getPosts() {
+  const posts = [
+    {
+      slug: "my-first-post",
+      title: "My first Post!",
+    },
+    {
+      slug: "trail-riding-with-onewheel",
+      title: "Trail riding with Onewheel!",
+    },
+  ];
+  return posts;
+}
